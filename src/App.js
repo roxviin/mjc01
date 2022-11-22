037f98d9ed2077cef0596b04cab54c9f0682ed9d
윤석빈 <yunseokbin@yunseogbin-ui-MacBookPro.local>
2022-11-23 00:30:13 +0900

    리액앱 업로드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from 'prop-types'
+
+function Food({name, image, rating}){
+  return(
+
+    <div>
+      <h1> I like {name} </h1>
+      <h4> {rating}/5.0 </h4>
+      <img src={image} art={name}/>
+    </div>
+    
+  
+  )
+};
+
+Food.propTypes = {
+  name:PropTypes.string.isRequired,
+  image:PropTypes.string.isRequired,
+  rating:PropTypes.number,
+}
+
+const foodILike = [
+  {
+    id: 1 ,
+    name:'kimchi' ,
+    image:'https://bit.ly/3d14HVx',
+    rating: 4.9,
+  },
+
+  {
+    id: 2 ,
+    name:'samgiopsal' ,
+    image:'http://asq.kr/wttMFjg1IkYC',
+    rating: 5, 
+  }
+];
+
+function renderFood(dish){
+  return(
+   
+      <Food key = {dish.id} name = {dish.name}
+             image = {dish.image} rating = {dish.rating} />
+          
+    )
+  }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     {foodILike.map(renderFood)}
+      </div>
   );
 }
 
